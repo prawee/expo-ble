31914fbc5dda19a22c089eca06dc0dfe9b975b12
Prawee Wongsa <prawee@hotmail.com>
2024-11-30 21:42:52 +0700

    using connectedDevice to display heart rate title

--- App.js
+++ App.js
@@ -4,10 +4,16 @@ import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
 import useBLE from './lib/ble';
 
 export default function App() {
-  const { color } = useBLE();
+  const { color, connectToDevice, connectedDevice } = useBLE();
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: color }]}>
-      <Text>Ble Solution!</Text>
+      <View style={styles.heartRateTitleWrapper}>
+        {connectedDevice ? (
+          <Text style={styles.heartRateTitleText}>Connected</Text>
+        ) : (
+          <Text style={styles.heartRateTitleText}>Please connect the Arduino</Text>
+        )}
+      </View>
       <StatusBar style="auto" />
     </SafeAreaView>
   );
@@ -17,5 +23,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#f2f2f2",
+  },
+  heartRateTitleWrapper: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  heartRateTitleText: {
+    fontSize: 30,
+    fontWeight: "bold",
+    textAlign: "center",
+    marginHorizontal: 20,
+    color: "black",
   },
 });
